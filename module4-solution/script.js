1cca223fb5d1fc5d38a96e4011db3b63388e0de0
ashwinspencer <57836872+ashwinspencer@users.noreply.github.com>
2021-01-16 12:59:52 -0600

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,9 +1,11 @@
-(function(){
-	var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-    for(var i=0;i<names.length;i++){
-    	if(names[i].charAt(0)==='j'||names[i].charAt(0)==='J')
-    		goodbyeOb.goodbye(names[i]);
-    	else
-    		helloOb.hello(names[i]);
+(function() {
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    for (var i = 0; i < names.length; i++) {
+        var firstLetter = names[i].charAt(0).toLowerCase();
+        if (firstLetter === 'j') {
+            byeSpeaker.speak(names[i]);
+        } else {
+            helloSpeaker.speak(names[i]);
+        }
     }
 })();
